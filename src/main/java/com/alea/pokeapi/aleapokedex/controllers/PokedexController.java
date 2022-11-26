f3b8b43c42f9d6da9efe886bd557329f37f03f00
Matias Bovina <matiasbovina@Mac-mini-de-Matias.local>
2022-11-26 10:23:11 +0100

    added get pokemon

--- src/main/java/com/alea/pokeapi/aleapokedex/controllers/PokedexController.java
+++ src/main/java/com/alea/pokeapi/aleapokedex/controllers/PokedexController.java
@@ -3,7 +3,10 @@ package com.alea.pokeapi.aleapokedex.controllers;
 import com.alea.pokeapi.aleapokedex.entity.Pokemon;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,13 +24,26 @@ public class PokedexController {
         this.restTemplate = restTemplate;
     }
 
+    @GetMapping(value = "/test/")
+    public Pokemon[] getAll() {
 
-    @GetMapping(value = "/test")
-    public List<Pokemon> testApi() {
-        String url = "https://pokeapi.co/api/v2/pokemon/1/";
-        List<Pokemon> pokemones = new ArrayList<>();
-         pokemones = Collections.singletonList(restTemplate.getForObject(url, Pokemon.class));
-         pokemones.stream();
+        ResponseEntity<Pokemon[]> response =
+                restTemplate.getForEntity("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5000",
+                        Pokemon[].class);
+        //String url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5000";
+        Pokemon[] pokemones = response.getBody();
+       // List<Pokemon> pokemones = new ArrayList<>();
+        //Object forObjects = Collections.restTemplate.getForObject(url, Object.class);
+
+        return pokemones;
+    }
+
+    @GetMapping(value = "/test/pokemon/{id}")
+    public Pokemon testApi(@PathVariable int id) {
+
+         String url = "https://pokeapi.co/api/v2/pokemon/" + id;
+         Pokemon pokemones = new Pokemon();
+         pokemones = restTemplate.getForObject(url, Pokemon.class);
         return pokemones;
     }
 
